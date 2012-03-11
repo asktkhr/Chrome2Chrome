1e508c25554d3746bea55a8e5c1bb1987d866672
asktkhr <bbclapton@gmail.com>
2012-03-11 21:48:58 +0900

    request push-server to send url

--- sender.js
+++ sender.js
@@ -1,6 +1,33 @@
 $(document).ready(
-   function(){
-     $("button#push").bind('click', function(event) {
-     });
-   }
+  function(){
+    var url = "";
+  　chrome.tabs.getSelected(null, function(tab) {
+      url = tab.url;
+    });
+
+    $('div#status').text(bg.status);
+
+    $("button#push").bind('click', function(event) {
+      $('div#status').text("seding...");
+      var button = $("button#push");
+      button.attr("disable","disable");
+      $.ajax({
+        url: "http://localhost:3000/websocket/send_url",
+        type: "post",
+        data: {
+          url: url,
+          socket_id: bg.socketId,
+          receivers: [] 
+        },
+        success: function(data) {
+          $('div#status').text(bg.status);
+          button.attr("disable","");
+        },
+        error: function(xhr, error) {
+          $('div#status').text(bg.status);
+          button.attr("disable","");
+        }
+      });
+    });
+  }
 );
