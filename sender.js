1b1fc5b45f528a79546a15d76da71d1109c5c6eb
asktkhr <bbclapton@gmail.com>
2012-03-19 12:17:20 +0900

    create device registration feature

--- sender.js
+++ sender.js
@@ -1,33 +1,68 @@
+function enablePushUrl(){
+  var url = "";
+　chrome.tabs.getSelected(null, function(tab) {
+    url = tab.url;
+  });
+  $('div#pc_name').remove()
+  $('div#container').prepend('<div id=status></div>')
+  $('div#status').text(bg.status);
+  $("button#push").text('push')
+  $("button#push").unbind('click')
+
+  $("button#push").bind('click', function(event) {
+    $('div#status').text("sending...");
+    var button = $("button#push");
+    button.attr("disabled","disabled");
+    $.post(
+      "http://localhost:3000/websocket/send_url",
+      {
+        url: url,
+        socket_id: bg.socketId,
+        receivers: [] 
+      },
+      function(data, status) {
+        if(status == 'success'){
+          $('div#status').text(bg.status);
+        }
+        else {
+          $('div#status').text(bg.status);
+        }
+        button.removeAttr("disabled");
+      },
+      "text"
+    );
+  });
+}
+
+
+function register(){
+  $("div#container").html("<div id='pc_name'>pc name: <input id=pc_name type='text'></div>");
+  $("div#status").remove();
+  $("button").text("register");
+  $("button").bind('click',function(event){
+    $.post(
+      "http://localhost:3000/websocket/register",
+      { name: $("input#pc_name").val() },
+      function(data, status) {
+        if(data == 'success') enablePushUrl();
+      },
+      "text"
+    );
+  });
+}
+
 $(document).ready(
   function(){
-    var url = "";
-  　chrome.tabs.getSelected(null, function(tab) {
-      url = tab.url;
-    });
-
-    $('div#status').text(bg.status);
-
-    $("button#push").bind('click', function(event) {
-      $('div#status').text("seding...");
-      var button = $("button#push");
-      button.attr("disable","disable");
-      $.ajax({
-        url: "http://localhost:3000/websocket/send_url",
-        type: "post",
-        data: {
-          url: url,
-          socket_id: bg.socketId,
-          receivers: [] 
-        },
-        success: function(data) {
-          $('div#status').text(bg.status);
-          button.attr("disable","");
-        },
-        error: function(xhr, error) {
-          $('div#status').text(bg.status);
-          button.attr("disable","");
+    $.get(
+      "http://localhost:3000/websocket/register_info",
+      function(data, status) {
+        if(data == null){
+          register();
         }
-      });
-    });
+        else {
+          enablePushUrl();
+        }
+      }
+    );
   }
 );
